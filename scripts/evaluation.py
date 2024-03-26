107aee1089b5aba7d93ab27f599d5cced75086e6
mhy-666 <57670156+mhy-666@users.noreply.github.com>
2024-03-26 02:10:15 +0000

    add evaluation.py

---
+++ scripts/evaluation.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import json
+
+class Evaluation:
+    def __init__(self, model_name = "gpt-4"):
+        self.model_name = model_name
+        self.client = OpenAI()
+
+    def evaluate(self):
+        response = self.client.chat.completions.create(
+        model = self.model_name,
+        # response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", "content": "You are a helpful poetry critic."},
+            {"role": "user", "content": "Which of the following 5 poems is most close to Bob Dylan's style? Give me the index, like the first/second/third..."
+            +list(self.poems_fine_tuned_gpt35[0])[0]+list(self.poems_fine_tuned_gpt35[0].values())[0]
+            +list(self.poems_fine_tuned_llama2[0])[0]+list(self.poems_fine_tuned_llama2[0].values())[0]
+            +list(self.poems_no_fine_tuned_gpt35[0])[0]+list(self.poems_no_fine_tuned_gpt35[0].values())[0]
+            +list(self.poems_no_fine_tuned_llama2[0])[0]+list(self.poems_no_fine_tuned_llama2[0].values())[0]
+            +list(self.poems_rag_gpt35[0])[0]+list(self.poems_rag_gpt35[0].values())[0]
+            }
+        ]
+        )
+        print(response.choices[0].message.content)
+        return response.choices[0].message.content
+
+
+    def load_test_data(self):
+        
+        with open('../output/poems_finetuned_gpt35.json', encoding='utf-8') as f:
+            self.poems_fine_tuned_gpt35 = json.load(f)
+
+        with open('../output/poems_finetuned_llama2.json', encoding='utf-8') as f:
+            self.poems_fine_tuned_llama2 = json.load(f)
+
+        with open('../output/poems_nofinetuned_gpt35.json', encoding='utf-8') as f:
+            self.poems_no_fine_tuned_gpt35 = json.load(f)
+
+        with open('../output/poems_nofinetuned_llama2.json', encoding='utf-8') as f:
+            self.poems_no_fine_tuned_llama2 = json.load(f) 
+
+        with open('../output/poems_rag_gpt35.json', encoding='utf-8') as f:
+            self.poems_rag_gpt35 = json.load(f)
+    
